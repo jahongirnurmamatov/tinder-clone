7bf84dfbbe3a341d908bd0cef408ec65ed27d9f4
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-10-26 23:38:12 +0500

    login, logout functions

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -39,7 +39,31 @@ const signup = async (req, res) => {
   }
 };
 
-const login = async (req, res) => {};
-const logout = async (req, res) => {};
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const user = await User.findOne({ email }).select("+password");
+    if (!user || (await user.matchPassword(password)) === false) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+    const token = signToken(user._id);
+    res.cookie("token", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+    });
+    res.status(200).json({ success: true, user });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+const logout = async (req, res) => {
+  res.clearCookie("token");
+  res.status(200).json({ success: true, message: "Logged out successfully" });
+};
 
 export { signup, login, logout };
