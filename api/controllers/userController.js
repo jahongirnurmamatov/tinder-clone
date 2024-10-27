00566aaca0fc6bea35b94ce339bfdf020706f22b
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-10-27 20:44:13 +0500

    updateProfile function

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,9 +1,38 @@
-
+import cloudinary from "../config/cloudinary.js";
+import User from "../models/User.js";
 
 export const updateProfile = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        
+  try {
+    const { image, ...otherData } = req.body;
+    let updatedData = otherData;
+    if (image) {
+      if (image.startsWith("data:image")) {
+        try {
+          const uploadResponse = await cloudinary.uploader.upload(image);
+          updatedData = { ...updatedData, image: uploadResponse.secure_url };
+        } catch (error) {
+          console.log(error.message);
+          return res
+            .status(400)
+            .json({ success: false, message: "Error in uploading image" });
+        }
+      }
     }
-}
+    const updatedUser = await User.findbyIdAndUpdate(
+      req.user._id,
+      updatedData,
+      { new: true }
+    );
+    res.status(200).json({
+      success: true,
+      user: updatedUser,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+   
