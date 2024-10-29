45c18d572b00963e8d71fff290407b76ef07be70
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-10-29 09:59:44 +0500

    folder structure of ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AuthPage from "./pages/AuthPage";
+import ProfilePage from "./pages/ProfilePage";
+import ChatPage from "./pages/ChatPage";
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-red-500 text-2xl'>Hello world</h1>
+    <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to-right, #f0f0f0_1px,transparent_1px) ,linear-gradient(to_bottom, #f0f0f0_1px, transparent_1px)] bg-[size:6rem_4rem]">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/chat/:id" element={<ChatPage />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
