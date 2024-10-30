c76d84e3e4e35e186ee76b5975fa02e197222125
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-10-30 09:33:08 +0500

    Sidebar of homepage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { useAuthStore } from '../store/useAuthStore'
-
+import React from "react";
+import { useAuthStore } from "../store/useAuthStore";
+import Sidebar from "../componenets/Sidebar";
 
 const HomePage = () => {
-    const {logout}= useAuthStore();
+  const { logout } = useAuthStore();
   return (
-    <div>
-        <button onClick={()=>logout()}>Logout</button>
+    <div className="flex flex-col lg:flex-row min-h-screen bg-gradient-to-br from-pink-100 to-purple-100 overflow-hidden">
+      <Sidebar />
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
