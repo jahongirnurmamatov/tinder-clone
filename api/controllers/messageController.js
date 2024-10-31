d3e5c5ff4c2a5ad9d6d73cfba1922535ac8a9bb5
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-10-31 11:08:15 +0500

    socket.io for message controller

--- api/controllers/messageController.js
+++ api/controllers/messageController.js
@@ -1,3 +1,5 @@
+import { getConnectedUsers, getIO } from "../socket/socket.server";
+
 export const sendMessage = async (req, res) => {
   try {
     const { content, receiverId } = req.body;
@@ -7,12 +9,20 @@ export const sendMessage = async (req, res) => {
       content,
     });
     // to do mesage in real time using socket io
+    const io = getIO();
+    const connectedUsers = getConnectedUsers();
+    const receiverSocketId = connectedUsers.get(receiverId);
+
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMesage", { message: newMessage });
+    }
+
     res.status(200).json({
       success: true,
       message: newMessage,
     });
   } catch (error) {
-    console.log(error.message)
+    console.log(error.message);
     res.status(500).json({
       success: false,
       message: error.message,
@@ -22,22 +32,22 @@ export const sendMessage = async (req, res) => {
 
 export const getConversation = async (req, res) => {
   try {
-    const {userId} = req.params;
+    const { userId } = req.params;
     const messages = await Message.find({
-        $or:[
-            {sender:userId,receiver:req.user._id},
-            {sender:req.user._id,receiver:userId}
-        ]
-    }).sort({createdAt:1});
+      $or: [
+        { sender: userId, receiver: req.user._id },
+        { sender: req.user._id, receiver: userId },
+      ],
+    }).sort({ createdAt: 1 });
     res.status(200).json({
       success: true,
-      messages
+      messages,
     });
   } catch (error) {
-    console.log(error.message)
+    console.log(error.message);
     res.status(500).json({
       success: false,
-      message: 'Internal Server Error',
+      message: "Internal Server Error",
     });
   }
 };
--- client/src/pages/ChatPage.jsx
+++ client/src/pages/ChatPage.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const ChatPage = () => {
   return (
-    <div>ChatPage</div>
+    <div>
+      
+    </div>
   )
 }
 
