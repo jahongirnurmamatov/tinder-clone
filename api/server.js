5c845035c5c032f20013f18f54379a0d525896e7
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-11-01 12:51:10 +0500

    server.js redeploy fix

--- api/server.js
+++ api/server.js
@@ -39,10 +39,10 @@ app.use("/api/matches", matchRouter);
 app.use("/api/messages", messagesRouter);
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'client/dist')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
   });
   
 }
